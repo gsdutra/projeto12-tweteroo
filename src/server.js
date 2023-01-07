aafe4304191deb80b91d08d788431cf442cdec73
dutra-86 <gabrielsprecherdutra@gmail.com>
2023-01-06 23:44:02 -0300

    Added header for username for post tweets method

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+//const { body, validationResult } = require('express-validator');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const PORT = 5000;
 
 var server_users = [];
 
@@ -13,20 +16,24 @@ app.get('/tweets', (req, res) => {
 	res.send(tweets.slice(-10).reverse());
 });
 
-app.post('/sign-up', (req, res) => {
-	const data = req.body;
-	server_users.push(data);
-	res.send('OK');
+app.post(
+	'/sign-up',(req, res) => {
+
+		const data = req.body;
+		server_users.push(data);
+		res.send('OK');
 });
 
 app.post('/tweets', (req, res) => {
 	const data = req.body;
+	const username = req.headers.user;
+	console.log(username);
 
-	if (server_users.find(element => element.username===data.username)){
+	if (server_users.find(element => element.username===username)){
 		tweets.push(
 			{
-				"username": data.username,
-				"avatar": server_users.find(({username})=> username === data.username).avatar,
+				"username": username,
+				"avatar": server_users.find(({username})=> username === username).avatar,
 				"tweet": data.tweet
 			}
 		)
@@ -37,4 +44,4 @@ app.post('/tweets', (req, res) => {
 });
 
 
-app.listen(5000,  ()=>console.log("App working"));
+app.listen(PORT,  ()=>console.log(`App running on port ${PORT}`));
