a5862075d897a172e008bc2215725f0ce7426d86
dutra-86 <gabrielsprecherdutra@gmail.com>
2023-01-07 01:02:29 -0300

    Fixed tweets order

--- src/server.js
+++ src/server.js
@@ -13,33 +13,47 @@ var server_users = [];
 var tweets = [];
 
 app.get('/tweets', (req, res) => {
-	res.send(tweets.slice(-10).reverse());
+	let page = req.query.page;
+	if(isNaN(parseInt(page))){page=1}
+	if(parseInt(page)>=1){
+		//res.send([...tweets].reverse().slice((page-1)*(10),page*10));
+		res.send([...tweets].reverse().slice((page-1)*(10),page*10).reverse());
+	}else{
+		res.status(400).send("Informe uma página válida!");
+	}
 });
 
 app.post(
 	'/sign-up',(req, res) => {
-
 		const data = req.body;
-		server_users.push(data);
-		res.send('OK');
+
+		if (data.username && data.avatar){
+			server_users.push(data);
+			res.send('OK')
+		}else{
+			res.status(400).send("Todos os campos são obrigatórios!");
+		}
 });
 
 app.post('/tweets', (req, res) => {
 	const data = req.body;
 	const username = req.headers.user;
-	console.log(username);
-
-	if (server_users.find(element => element.username===username)){
-		tweets.push(
-			{
-				"username": username,
-				"avatar": server_users.find(({username})=> username === username).avatar,
-				"tweet": data.tweet
-			}
-		)
-		res.send("OK");
+	
+	if (data.tweet && username){
+		if (server_users.find(element => element.username===username)){
+			tweets.push(
+				{
+					"username": username,
+					"avatar": server_users.find(({username})=> username === username).avatar,
+					"tweet": data.tweet
+				}
+			)
+			res.status(201).send("OK");
+		}else{
+			res.status(400).send("UNAUTHORIZED");
+		}
 	}else{
-		res.send("UNAUTHORIZED");
+		res.status(400).send("Todos os campos são obrigatórios!");
 	}
 });
 
